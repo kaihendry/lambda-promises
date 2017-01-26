6773d480448896eec35e48397d760fbcd5bb601f
Kai Hendry <hendry@webconverger.com>
2017-01-26 16:24:46 +0800

    got it

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var p1 = require('./p1.js')();
+var p1 = require('./p1.js');
 var p2 = require('./p2.js');
 var p3 = require('./p3.js');
 var p4 = require('./p4.js');
 
 console.log(p1, p2, p3, p4)
 
-p1
+p1()
 .then(p2)
 .then(p3)
 .then(p4)
