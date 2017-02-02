667bdf67421c18b52dcdb9fd7a967fa3bdd30519
Kai Hendry <hendry@iki.fi>
2017-02-02 14:56:35 +0800

    Tidy up

--- handler.js
+++ handler.js
@@ -20,22 +20,16 @@ module.exports.promise = (event, context, callback) => {
 	const randomNumber = Math.random()
 
 	const params = { TableName: process.env.TABLE, ReturnValues: "ALL_NEW", Key: { uuid: uuidgen }, AttributeUpdates: { randomNumber: { Action: 'PUT', Value: randomNumber }}}
-	dynamodb.update(params, (err, data) => {
-		if (err) console.log(err);
-		else {
-			console.log("Update CB", data)
-			assert(uuidgen, data.Attributes.uuid)
-		}
-		});
 
 	dynamodb.update(params).promise()
 		.then((data) => { console.log("After update", data);
+			assert(uuidgen, data.Attributes.uuid)
 			dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: uuidgen }}).promise()
 				.then((data) => { console.log("After get", data);
 					assert(uuidgen, data.Item.uuid);
 					return callback(null, { message: data })
 				})
-				.catch(function(err) { console.error('Failed to get:' + err); })
+				.catch((err) => { console.error('Failed to get:' + err); })
 		})
 
 };
