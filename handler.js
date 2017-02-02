6767fe635ce535317bd9f71db5de4419b9825632
Kai Hendry <hendry@iki.fi>
2017-02-02 14:48:28 +0800

    TIL ReturnValues

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 const aws = require('aws-sdk');
 const uuid = require('node-uuid');
+const assert = require('assert');
 
 aws.config.update({
 	region: 'ap-southeast-1'
@@ -12,21 +13,26 @@ module.exports.promise = (event, context, callback) => {
 
 	// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
 	dynamodb.scan({ TableName: process.env.TABLE }).promise()
-	.then((data) => { console.log("After scan", data); })
-	.catch(function(err) { console.error('Failed to scan:' + err); })
+		.then((data) => { console.log("After scan", data); })
+		.catch(function(err) { console.error('Failed to scan:' + err); })
 
 	const uuidgen = uuid.v4()
 	const randomNumber = Math.random()
 
-	dynamodb.update({ TableName: process.env.TABLE, Key: { uuid: uuidgen }, AttributeUpdates: { randomNumber: { Action: 'PUT', Value: randomNumber }}}).promise()
-	.then((data) => { console.log("After put", data); })
-	.catch(function(err) { console.error('Failed to put:' + err); })
+	const params = { TableName: process.env.TABLE, ReturnValues: "ALL_NEW", Key: { uuid: uuidgen }, AttributeUpdates: { randomNumber: { Action: 'PUT', Value: randomNumber }}}
+	dynamodb.update(params, (err, data) => {
+		if (err) console.log(err);
+		else console.log("Update CB", data)
+		});
 
-	dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: uuidgen }}).promise()
-	.then((data) => { console.log("After get", data); return callback(null, { message: data }) })
-	.catch(function(err) { console.error('Failed to get:' + err); })
-
-
-	// return callback(null, { message: 'Hi there', event });
+	dynamodb.update(params).promise()
+		.then((data) => { console.log("After update", data);
+			dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: uuidgen }}).promise()
+				.then((data) => { console.log("After get", data);
+					assert(uuidgen, data.Item.uuid);
+					return callback(null, { message: data })
+				})
+				.catch(function(err) { console.error('Failed to get:' + err); })
+		})
 
 };
