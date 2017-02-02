b86ce3c953086e335ce195dec4a0685981aaf712
Kai Hendry <hendry@iki.fi>
2017-02-02 14:32:45 +0800

    returns on get, sometimes

--- handler.js
+++ handler.js
@@ -8,38 +8,25 @@ aws.config.update({
 console.log("AWS version", aws.VERSION)
 const dynamodb = new aws.DynamoDB.DocumentClient();
 
-function getItem(params) { return dynamodb.get(params).promise() }
-function putItem(params) { return dynamodb.update(params).promise() }
-
 module.exports.promise = (event, context, callback) => {
 
 	// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-	dynamodb.scan({ TableName: process.env.TABLE }, function(err, data) {
-		if (err) console.log("Callback scan error", err);
-		else console.log("Callback scan", data);
-	});
-
-	// Using a Promise
-	dynamodb.scan({ TableName: process.env.TABLE }).promise()
-	.then(function(data) {
-		  console.log('Promise Scan Success', data);
-	}).catch(function(err) {
-		  console.log("Promise scan error", err);
-	});
-
-	// Using a Promise, shorter
 	dynamodb.scan({ TableName: process.env.TABLE }).promise()
 	.then((data) => { console.log("After scan", data); })
+	.catch(function(err) { console.error('Failed to scan:' + err); })
 
 	const uuidgen = uuid.v4()
 	const randomNumber = Math.random()
 
 	dynamodb.update({ TableName: process.env.TABLE, Key: { uuid: uuidgen }, AttributeUpdates: { randomNumber: { Action: 'PUT', Value: randomNumber }}}).promise()
 	.then((data) => { console.log("After put", data); })
+	.catch(function(err) { console.error('Failed to put:' + err); })
 
-	// putItem({ TableName: process.env.TABLE, Key: { uuid: uuidgen }, AttributeUpdates: { randomNumber: { Action: 'PUT', Value: randomNumber }}})
-	//.then((data) => { console.log("After put", data); getItem({ TableName: process.env.TABLE, Key: { uuid: uuidgen }}) })
-	//.then((data) => { console.log("After get", data, uuidgen); return callback(null, { message: data, event }) })
-	//.catch(function(err) { console.error('Uh oh:' + err); })
+	dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: uuidgen }}).promise()
+	.then((data) => { console.log("After get", data); return callback(null, { message: data }) })
+	.catch(function(err) { console.error('Failed to get:' + err); })
+
+
+	// return callback(null, { message: 'Hi there', event });
 
 };
