ff0eebe37a5721e0d0a59a54fc831a48eace790c
Kai Hendry <hendry@iki.fi>
2017-02-03 09:39:24 +0800

    Probably wise not to return Trying out "standard"

--- handler.js
+++ handler.js
@@ -1,29 +1,29 @@
-const aws = require('aws-sdk');
-const uuid = require('node-uuid');
-const assert = require('assert');
+const aws = require('aws-sdk')
+const uuid = require('node-uuid')
+const assert = require('assert')
 
 aws.config.update({
-	region: 'ap-southeast-1',
-});
+  region: 'ap-southeast-1'
+})
 
 console.log('AWS version', aws.VERSION)
-const dynamodb = new aws.DynamoDB.DocumentClient();
+const dynamodb = new aws.DynamoDB.DocumentClient()
 
 module.exports.promised = (event, context, callback) => {
-	const uuidgen = uuid.v4()
-	const randomNumber = Math.random()
+  const uuidgen = uuid.v4()
+  const randomNumber = Math.random()
 
-	const params = { TableName: process.env.TABLE, ReturnValues: 'ALL_NEW', Key: { uuid: uuidgen }, AttributeUpdates: { randomNumber: { Action: 'PUT', Value: randomNumber } } }
+  const params = { TableName: process.env.TABLE, ReturnValues: 'ALL_NEW', Key: { uuid: uuidgen }, AttributeUpdates: { randomNumber: { Action: 'PUT', Value: randomNumber } } }
 
-	return dynamodb.update(params).promise()
-	.then((data) => {
-		console.log('After update', data);
-		assert.equal(uuidgen, data.Attributes.uuid)
-		return dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: uuidgen } }).promise()
-	})
-	.then((data) => {
-		console.log('After get', data);
-		assert.equal(uuidgen, data.Item.uuid);
-		callback(null, { message: data })
-	})
-};
+  dynamodb.update(params).promise()
+  .then((data) => {
+    console.log('After update', data)
+    assert.equal(uuidgen, data.Attributes.uuid)
+    return dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: uuidgen } }).promise()
+  })
+  .then((data) => {
+    console.log('After get', data)
+    assert.equal(uuidgen, data.Item.uuid + 'omg')
+    callback(null, { message: data })
+  }).catch((error) => { callback(error) })
+}
