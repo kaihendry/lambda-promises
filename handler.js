1ab133f8dac2690c01cbdb32391d60acd403b2d6
Kai Hendry <hendry@iki.fi>
2017-02-08 11:56:01 +0800

    Something wrong

--- handler.js
+++ handler.js
@@ -23,25 +23,14 @@ module.exports.promised = (event, context, callback) => {
       randomNumber: { Action: 'PUT', Value: randomNumber }
     } }
 
-  Promise.resolve().then(() => { // depending on event type, we start the chain with a certain promise or not
-    if (!event) return // resolves `undefined`
-    return Promise.resolve('doanupdate')
-  })
-  .then((data) => { // if that certain promise was run, we then update the entry
-    if (data) {
-      console.log('Doing an update!')
-      return dynamodb.update(params).promise()
-    }
-    return
-  })
-  .then((data) => {
-    if (data) {
-      console.log('Done an update', data)
+  return (
+    event ? dynamodb.update(params).promise().then((data) => {
+      console.log('Doing an update', data)
       assert.equal(uuidgen, data.Attributes.uuid)
-    }
-    console.log('Going to fetch', uuidgen)
-    return dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: uuidgen } }).promise()
-  })
+      console.log('uuid', uuidgen)
+    }).then(dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: '980a157d-05e5-4f7f-b6af-00392d12ce56'} }).promise()) // <-- this promise is returning undefined!!
+    : dynamodb.get({ TableName: process.env.TABLE, Key: { uuid: '980a157d-05e5-4f7f-b6af-00392d12ce56' } }).promise()
+  )
   .then((data) => {
     console.log('Done a get', data)
     // HALP? Assert only if we did an update earlier
